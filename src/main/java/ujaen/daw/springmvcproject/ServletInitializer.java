5d04a8daca557b1fbafdce04735dd809256fb8c0
José Ramón Balsas <jrbalsas@ujaen.es>
2024-03-12 12:09:09 +0100

    Do it!

---
+++ src/main/java/ujaen/daw/springmvcproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ujaen.daw.springmvcproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringmvcProjectApplication.class);
+    }
+
+}
---
+++ src/test/java/ujaen/daw/springmvcproject/SpringmvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ujaen.daw.springmvcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
